645dbe245db65183e69d020b9874094c0f9f8874
Ross Johnson <ross@mazira.com>
2013-07-09 14:03:12 -0500

    Add lowercase encode / decode properties

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+var Base64Encode = require('./lib/encode');
+var Base64Decode = require('./lib/decode');
+
 module.exports = {
-	Encode : require('./lib/encode.js'),
-	Decode : require('./lib/decode.js')
+	Encode : Base64Encode,
+	encode : Base64Encode,
+	Decode : Base64Decode,
+	decode : Base64Decode
 };
